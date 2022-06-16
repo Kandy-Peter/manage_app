effdd1134c000ccc8f865060e075896e990cf918
kandy-peter <kandypeter03@gmail.com>
2022-06-16 23:53:15 +0200

    Generate Sale list view

--- src/sales/urls.py
+++ src/sales/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import home_view
+from .views import (
+  home_view,
+  SaleListView,
+)
 
 app_name = 'sales'
 
 urlpatterns = [
   path('', home_view, name = 'home'),
+  path('list/', SaleListView.as_view(), name = 'list'), #suer class.as_view() as it's a class view
 ]
-
--- src/sales/views.py
+++ src/sales/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Sale
 
 # Create your views here.
 
 def home_view(request):
   hello = 'Hello world from the view'
-  return render(request, 'sales/main.html', {'hello': hello})
+  return render(request, 'sales/home.html', {'hello': hello})
+
+class SaleListView(ListView):
+  model = Sale
+  template_name = 'sales/main.html'
+  # I can object_context_name = "sale" and this will overide the 'object_list' in main.html
+  
