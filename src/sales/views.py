64e412f1ad1bff8e56aad176b04142979eb0cc89
kandy-peter <kandypeter03@gmail.com>
2022-06-17 02:30:14 +0200

    Display dataframe to the view tmeplate

--- src/sales/views.py
+++ src/sales/views.py
@@ -6,21 +6,30 @@ from .forms import SalesSearchForm
 # Create your views here.
 
 def home_view(request):
+  sales_df = None
   form = SalesSearchForm(request.POST or None)
-  
   if request.method == 'POST':
     date_from = request.POST.get('date_from')
     date_to = request.POST.get('date_to')
     chart_type = request.POST.get('chart_type')
     print(date_from, date_to, chart_type)
 
-    qs = Sale.objects.filter(created__date=date_from)
-    print("#################")
-    df1 =  pd.DataFrame(qs.values())
-    print(df1)
+    qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from) #__lte: created date less than or equal to date_to
+    
+    if len(qs) > 0:
+      print("#################")
+      sales_df =  pd.DataFrame(qs.values())
+      
+      #to avoid ambiguous truth value of dataframe
+
+      sales_df = sales_df.to_html()
+      print(sales_df)
+    else:
+      print('no data')
   # generate the context dictionary
   context = {
     'form': form,
+    'sales_df': sales_df,
   }
   return render(request, 'sales/home.html', context)
 
